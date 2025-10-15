20471053a3863eae80decc9113a8b812e0071046
Andres. R <amromero2005@gmail.com>
2025-10-15 19:17:35 -0400

    Started "database" for all moves in pokemon

--- MoveDeclarations.h
+++ MoveDeclarations.h
@@ -2,4 +2,11 @@
 
 #pragma once
 
+// For testing purposes I'm only including gen 1 moves
+
 // Normal Type Moves
+Move barrageMove("Barrage", 15, 85, "Physical", "None", normalType, "Hits the target 2-5 times in one turn.", 20);
+Move bideMove("Bide", 0, 100, "Physical", "None", normalType, "Endures attacks for two turns, then strikes back double the damage taken.", 10);
+Move bindMove("Bind", 15, 85, "Physical", "None", normalType, "Traps the target and squeezes for 4-5 turns.", 15);
+Move bodySlamMove("Body Slam", 85, 100, "Physical", "Paralyze", normalType, "A full-body slam that may also paralyze the target.", 15);
+Move cometPunchMove("Comet Punch", 18, 85, "Physical", "None", normalType, "Hits the target 2-5 times in one turn.", 15);
--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include "TypeDeclarations.h"
 
 int main() {
-    Move flamethrowerMove("Flamethrower", 90, 100, "Special", "Burn", fireType, "A powerful fire attack that may inflict a burn.");
-    Move flyMove("Fly", 90, 95, "Physical", "None", flyingType, "Flies up high on the first turn, then strikes on the second turn.");
-    Move slashMove("Slash", 70, 100, "Physical", "None", normalType, "A quick, slashing attack with a high critical-hit ratio.");
-    Move fireBlastMove("Fire Blast", 110, 85, "Special", "Burn", fireType, "A fiery blast that scorches all. May also leave the target with a burn.");
+    Move flamethrowerMove("Flamethrower", 90, 100, "Special", "Burn", fireType, "A powerful fire attack that may inflict a burn.", 15);
+    Move flyMove("Fly", 90, 95, "Physical", "None", flyingType, "Flies up high on the first turn, then strikes on the second turn.", 15);
+    Move slashMove("Slash", 70, 100, "Physical", "None", normalType, "A quick, slashing attack with a high critical-hit ratio.", 20);
+    Move fireBlastMove("Fire Blast", 110, 85, "Special", "Burn", fireType, "A fiery blast that scorches all. May also leave the target with a burn.", 5);
     Pokemon charizard("Charizard", 36, fireType, flyingType, 150, 0, flamethrowerMove, flyMove, slashMove, fireBlastMove);
     charizard.printPokemon();
 
-    Move hydroPumpMove("Hydro Pump", 110, 80, "Special", "None", waterType, "Blasts water at high power to damage the opponent.");
-    Move surfMove("Surf", 90, 100, "Special", "None", waterType, "A wave crashes down on the target.");
-    Move biteMove("Bite", 60, 100, "Physical", "Flinch", darkType, "Bites the target.");
-    Move iceBeamMove("Ice Beam", 90, 100, "Special", "Freeze", iceType, "Fires a beam of ice at the target.");
+    Move hydroPumpMove("Hydro Pump", 110, 80, "Special", "None", waterType, "Blasts water at high power to damage the opponent.", 5);
+    Move surfMove("Surf", 90, 100, "Special", "None", waterType, "A wave crashes down on the target.", 15);
+    Move biteMove("Bite", 60, 100, "Physical", "Flinch", darkType, "Bites the target.", 25);
+    Move iceBeamMove("Ice Beam", 90, 100, "Special", "Freeze", iceType, "Fires a beam of ice at the target.", 10);
     Pokemon blastoise("Blastoise", 36, waterType, Type(), 155, 0, hydroPumpMove, surfMove, biteMove, iceBeamMove);
     blastoise.printPokemon();
 
